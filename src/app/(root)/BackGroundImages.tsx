f57039506a421edcfec28c16c60384c285d30b56
kirito <kirito666codr@gmail.com>
2026-01-19 22:59:54 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/(root)/BackGroundImages.tsx
+++ src/app/(root)/BackGroundImages.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function BackGroundImages() {
   return (
     <div className="images">
-      {["tommorowland", "navy-pier", "msi-chicago", "phone", "kikk", "kennedy", "opera"].map(
+      {["tomorrowland", "navy-pier", "msi-chicago", "phone", "kikk", "kennedy", "opera"].map(
         item => (
-          <Image key={item} src={`/${item}.png`} alt={item} height={2000} width={2000} />
+          <Image key={item} id={item} src={`/${item}.png`} alt={item} height={2000} width={2000} />
         )
       )}
     </div>
