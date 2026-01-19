6238b0d028851a36d881deed2a34c4494ba36677
kirito <kirito666codr@gmail.com>
2026-01-19 17:16:56 +0530

    Enhance ProjectSection layout and styling with titles and hover effects

--- src/app/(root)/ProjectSection.tsx
+++ src/app/(root)/ProjectSection.tsx
@@ -1,7 +1,36 @@
 export default function ProjectSection() {
   return (
     <section id="section-2">
-      <div>hello</div>
+      <div className="titles">
+        <div img-title="tomorrowland" className="title">
+          <small>2020 - ONGOING</small>
+          <h1>Tomorrowland</h1>
+        </div>
+        <div img-title="navy-pier" className="title">
+          <small>2020 - ONGOING</small>
+          <h1> Navy Pier</h1>
+        </div>
+        <div img-title="msi-chicago" className="title">
+          <small>2020 - ONGOING</small>
+          <h1> MSI Chicago</h1>
+        </div>
+        <div img-title="phone" className="title">
+          <small>2020 - ONGOING</small>
+          <h1>This Was Louise’s Phone</h1>
+        </div>
+        <div img-title="kikk" className="title">
+          <small>2020 - ONGOING</small>
+          <h1>KIKK Festival 2018</h1>
+        </div>
+        <div img-title="kennedy" className="title">
+          <small>2020 - ONGOING</small>
+          <h1>The Kennedy Center</h1>
+        </div>
+        <div img-title="opera" className="title">
+          <small>2020 - ONGOING</small>
+          <h1>Royal Opera Of Wallonia</h1>
+        </div>
+      </div>
     </section>
   );
 }
